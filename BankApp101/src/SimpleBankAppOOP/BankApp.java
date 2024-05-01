2c3ba4d6b39f1fa7c9076d6a0089c8b6de864346
ismayilSDET <ismayil.jumshudov@gmail.com>
2024-05-01 00:16:49 -0400

    A simple OOP for Library..Initial Commit.

--- BankApp101/src/SimpleBankAppOOP/BankApp.java
+++ BankApp101/src/SimpleBankAppOOP/BankApp.java
@@ -10,7 +10,7 @@ class BankApp {
 
 
         Transaction.deposit(accounts[0],500.00);
-        Transaction.withdraw(accounts[1],25.00);
+        Transaction.withdraw(accounts[1],11.00);
 
         System.out.println("Balance of account 755: $" + accounts[0].getBalance());
         System.out.println("Balance of account 800: $" + accounts[1].getBalance());
